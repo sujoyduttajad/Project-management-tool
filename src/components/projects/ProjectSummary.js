e4624966fbf627cfc6a3888afdfc4e198f6c6396
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-05-11 15:54:52 +0530

    Update ProjectSummary.js

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,19 +1,18 @@
-import React from 'react'
-import moment from 'moment';
-
+import React from "react";
+import moment from "moment";
 
 function ProjectSummary({ project }) {
-    return (
-        <div>
-            <div className="card z-depth-0 project-summary card-layout hoverable">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">{project.title}</span>
-                    <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
-                    <p className="grey-text">{moment(project.createdAt.toDate()).calendar()}</p>
-                </div>
-            </div>
-        </div>
-    )
+  return (
+    <div className="card-layout">
+      <h4 className="card-title">{project.title}</h4>
+      <p>
+        Posted by {project.authorFirstName} {project.authorLastName}
+      </p>
+      <p className="grey-text">
+        {moment(project.createdAt.toDate()).calendar()}
+      </p>
+    </div>
+  );
 }
 
 export default ProjectSummary;
