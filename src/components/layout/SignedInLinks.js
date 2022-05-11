f6da03896f9bc68587c284b8d7f1e898b4ec3b1b
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-05-11 15:23:01 +0530

    Update SignedInLinks.js

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -3,19 +3,20 @@ import { NavLink } from "react-router-dom";
 import { connect } from "react-redux";
 import { signOut } from "../../store/actions/authActions";
 import { Button } from "@mui/material";
+import {BiMessageSquareAdd} from 'react-icons/bi'
 
 function SignedInLinks(props) {
   return (
     <ul className="right">
-      <li className="tab">
-        <Button variant="contained">
+      <li>
+        <Button variant="contained" startIcon={<BiMessageSquareAdd />}>
           <NavLink to="/create">New Project</NavLink>
         </Button>
       </li>
       <li className="tab">
         <a onClick={props.signOut}>Log Out</a>
       </li>
-      <li>
+      <li className="tab">
         <NavLink
           to="/"
           className="btn btn-floating grey lighten-5 text darken-4"
