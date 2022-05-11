1cbf42059847bc1a6a6c799d0e2a5588fdca0309
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-05-11 18:07:27 +0530

    Update SignedInLinks.js

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -2,31 +2,50 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { connect } from "react-redux";
 import { signOut } from "../../store/actions/authActions";
-import { Avatar, Button } from "@mui/material";
+import { Avatar, Button, capitalize } from "@mui/material";
+import { styled } from '@mui/material/styles';
 import { BiMessageSquareAdd } from "react-icons/bi";
+
+export const SecondaryButton = styled(Button)({
+    fontFamily: "'Noto Sans', sans-serif",
+    fontSize: '1.1em',
+    fontWeight: 600,
+    width: '100%',
+    textTransform: "uppercase",
+    color: '#131E3F',
+    backgroundColor: '#E7ECF8',
+    margin: '0.6em 0',
+    padding: '0.2em 0.7em',
+    "&:hover": {
+        backgroundColor: '#D0D8F1'
+    },
+  });
 
 function SignedInLinks(props) {
   return (
-    <ul className="right">
-      <li>
-        <Button variant="contained" startIcon={<BiMessageSquareAdd />}>
-          <NavLink to="/create">New Project</NavLink>
-        </Button>
-      </li>
-     
-      <li className="tab">
-        <Avatar>
-          <NavLink to="/" className="">
-            {props.profile.initials}
-          </NavLink>
-        </Avatar>
-      </li>
-      <li className="tab">
-        <Button>
-          <a onClick={props.signOut}>Log Out</a>
-        </Button>
-      </li>
-    </ul>
+    <div className="sidebar-column">
+      <ul className="right">
+        <li>
+          <Button variant="contained" startIcon={<BiMessageSquareAdd />}>
+            <NavLink to="/create">New Project</NavLink>
+          </Button>
+        </li>
+      </ul>
+      <ul className="right">
+        <li className="tab">
+          <Avatar>
+            <NavLink to="/" className="">
+              {props.profile.initials}
+            </NavLink>
+          </Avatar>
+        </li>
+        <li className="tab">
+          <SecondaryButton variant="outlined">
+            <a onClick={props.signOut}>Log Out</a>
+          </SecondaryButton>
+        </li>
+      </ul>
+    </div>
   );
 }
 
