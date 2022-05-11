11a4abe9d15003142742813a053514587898374e
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-05-11 15:54:54 +0530

    Update ProjectList.js

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,21 +1,20 @@
-import React from 'react'
-import ProjectSummary from './ProjectSummary';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import ProjectSummary from "./ProjectSummary";
+import { Link } from "react-router-dom";
 
 function ProjectList({ projects }) {
-    return (
-    
-        <div className="project-list section">
-            { projects && projects.map(project => {
-                return (
-                <Link to={'/project/' + project.id} key={project.id}>  
-                    <ProjectSummary project={project}  />
-                </Link>
-                )
-            })}
-        </div>
-    )
+  return (
+    <section className="project-list-section">
+      {projects &&
+        projects.map((project) => {
+          return (
+            <Link to={"/project/" + project.id} key={project.id}>
+              <ProjectSummary project={project} />
+            </Link>
+          );
+        })}
+    </section>
+  );
 }
 
 export default ProjectList;
