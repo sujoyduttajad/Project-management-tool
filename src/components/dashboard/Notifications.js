7e35f38f68b1b332a8e15c6fc34fb995014a64c3
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-05-12 14:37:05 +0530

    Update Notifications.js

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,15 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Slide from "@mui/material/Slide";
 import Snackbar from "@mui/material/Snackbar";
 import { styled } from "@mui/material/styles";
+import { IconButton } from "@mui/material";
+import { MdClose } from "react-icons/md";
+import MuiAlert from "@mui/material/Alert";
 
-export const NotifyBar = styled(Snackbar)({
-  "& .MuiSnackbarContent-root": {
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <NotifyBar elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+export const NotifyBar = styled(MuiAlert)({
+ 
     fontFamily: "'Noto Sans', sans-serif",
     fontSize: "1em",
     color: "#E7ECF8",
     backgroundColor: "#131E3F",
-  },
+  
 });
 
 function Notifications(props) {
@@ -18,12 +25,15 @@ function Notifications(props) {
     Transition: Slide,
   });
 
-  const handleOpen = () => {
-    setState({
-      ...state,
-      open: true,
-    });
-  };
+  useEffect(() => {
+    const handleOpen = () => {
+      setState({
+        ...state,
+        open: true,
+      });
+    };
+    handleOpen();
+  }, []);
 
   const handleClose = () => {
     setState({
@@ -34,24 +44,37 @@ function Notifications(props) {
 
   return (
     <div className="section ">
-      <NotifyBar
-        open={handleOpen}
-        transitionDuration={1000}
-        autoHideDuration={100}
+      <Snackbar
+        open={state.open}
+        autoHideDuration={8000}
         onClose={handleClose}
         TransitionComponent={state.Transition}
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-        message={
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        key={state.Transition.name}
+        action={
+          <IconButton
+            aria-label="close"
+            onClick={handleClose}
+            style={{
+              color: "#fff",
+              position: "relative",
+              top: "-30px",
+            }}
+          >
+            <MdClose />
+          </IconButton>
+        }
+      >
+        <Alert onClose={handleClose} severity="info" sx={{ width: "100%" }}>
           <div className="notification-container">
             <h3>Notifications</h3>
             <ul>
-              <li>Notifications are still in development</li>
-              <li>Soon to be deployed with notifications</li>
+              <li>Notifications are still in development.</li>
+              <li>It will be available soon!!</li>
             </ul>
           </div>
-        }
-        key={state.Transition.name}
-      />
+        </Alert>
+      </Snackbar>
     </div>
   );
 }
