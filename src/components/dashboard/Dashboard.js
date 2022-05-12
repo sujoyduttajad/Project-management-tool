cf1b4652865c2f7b22b3d0862c7a6a54b7b6af99
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-05-12 13:48:51 +0530

    Update Dashboard.js

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -24,7 +24,6 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = (state) => {
-  console.log(state);
   return {
     projects: state.firestore.ordered.projects,
     auth: state.firebase.auth,
