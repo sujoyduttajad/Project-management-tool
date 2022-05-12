e154ae24b0167130c9765052a04606da91c5c2c9
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-05-12 17:20:47 +0530

    Update ProjectSummary.js

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,16 +1,64 @@
 import React from "react";
 import moment from "moment";
+import { Card, CardActions, CardContent, Typography, Button } from "@mui/material";
+import { styled } from "@mui/material/styles";
+// import { SecondaryButton } from "../layout/SignedInLinks";
+
+export const CustomCard = styled(Card)({
+    fontFamily: "'Noto Sans', sans-serif",
+    width: '100%',
+    
+    "&:hover": {
+      boxShadow: "-1px 3px 8px 0px rgba(56,56,56,0.91)",
+    },
+});
+
+export const CustomTypo = styled(Typography)({
+  fontFamily: "'Noto Sans', sans-serif",
+});
+
+export const ExpandButton = styled(Button)({
+  fontFamily: "'Noto Sans', sans-serif",
+  fontSize: "1sem",
+  fontWeight: 600,
+  width: "100%",
+  textTransform: "uppercase",
+  color: "#131E3F",
+  backgroundColor: "#E7ECF8",
+  margin: "0.6em 0",
+  padding: "0.2em 0.7em",
+  borderColor: "#131E3F",
+  "&:hover": {
+    backgroundColor: "#D0D8F1",
+    borderColor: "#131E3F",
+    boxShadow: "none",
+  },
+});
 
 function ProjectSummary({ project }) {
+  console.log(project);
   return (
     <div className="card-layout">
-      <h4 className="card-title">{project.title}</h4>
-      <p>
-        Posted by {project.authorFirstName} {project.authorLastName}
-      </p>
-      <p className="grey-text">
-        {moment(project.createdAt.toDate()).calendar()}
-      </p>
+      <CustomCard>
+        <CardContent>
+          <CustomTypo sx={{ fontSize: '2em' }} variant="h4">{project.title}</CustomTypo>
+          <CustomTypo variant="body1">
+            Posted by {project.authorFirstName} {project.authorLastName}
+          </CustomTypo>
+          <CustomTypo variant="body2">
+            {moment(project.createdAt.toDate()).calendar()}
+          </CustomTypo>
+        </CardContent>
+        <CardActions>
+        <ExpandButton 
+          disableElevation
+          disableFocusRipple
+          size="small"
+          >
+            Show More
+          </ExpandButton>
+        </CardActions>
+      </CustomCard>
     </div>
   );
 }
