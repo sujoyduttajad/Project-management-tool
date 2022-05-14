678e6959c32997e03e21f78dad7baf17ea706fcc
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-05-14 16:31:46 +0530

    Update SignedOutLinks.js

--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -2,15 +2,15 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { PrimaryButton, SecondaryButton } from "./SignedInLinks";
 
-
 function SignedOutLinks() {
-
-
   return (
     <div className="sidebar-column">
       <ul className="right">
         <li className="tab">
-          <PrimaryButton variant="outlined" >
+          <PrimaryButton
+            variant="contained"
+            style={{ width: "10em", fontSize: "1em", textTransform: 'uppercase' }}
+          >
             <NavLink to="/signup">Sign Up</NavLink>
           </PrimaryButton>
         </li>
