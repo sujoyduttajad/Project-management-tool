b35291e74713070d49933e40d0212756dd2cc96c
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-05-14 19:07:06 +0530

    Update Navbar.js

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
 import SignedInLinks from "./SignedInLinks";
 import SignedOutLinks from "./SignedOutLinks";
 import { connect } from "react-redux";
-import collab from "../../images/collab.svg";
 
 function Navbar(props) {
   const { auth, profile } = props;
@@ -13,18 +12,7 @@ function Navbar(props) {
     <SignedOutLinks />
   );
 
-  return (
-    <nav className="nav-wrapper">
-      <div className="container">
-        <div className="brand-logo">
-          <Link to="/" >
-            <img src={collab} alt="Brand-logo" />
-          </Link>
-        </div>
-        {links}
-      </div>
-    </nav>
-  );
+  return <nav className="">{links}</nav>;
 }
 
 const mapStateToProps = (state) => {
