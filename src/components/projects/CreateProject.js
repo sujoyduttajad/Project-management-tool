d2e7482374716015cbcfa380036b052696023660
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-05-15 20:53:45 +0530

    Update CreateProject.js

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { createProjectAction } from "../../store/actions/projectActions";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
+import { CustomCard, CustomTypo } from "./ProjectSummary";
+import { CardContent } from "@mui/material";
+
 
 class CreateProject extends Component {
   state = {
@@ -21,27 +24,44 @@ class CreateProject extends Component {
   render() {
     const { auth } = this.props;
     if (!auth.uid) return <Redirect to="/signin" />;
+
     return (
       <div className="feed-section">
-        <form onSubmit={this.handleSubmit} className="white">
-          <h5 className="">Create New Project</h5>
-          <div className="input-field">
-            <label htmlFor="title">Title</label>
-            <input type="text" id="title" onChange={this.handleChange} />
-          </div>
-          <div className="input-field">
-            <label htmlFor="content">Content</label>
-            <textarea
-              className="materialize-textarea"
-              id="content"
-              onChange={this.handleChange}
-            ></textarea>
-          </div>
+        <div className="project-detail">
+        <h1 className="project-header">Create New Project</h1>
+          <CustomCard>
+            <CardContent>
+              <form onSubmit={this.handleSubmit}>
+                <div className="input-container">
+                  <div className="input-field">
+                    <label htmlFor="title">Title</label>
+                    <input
+                      type="text"
+                      id="title"
+                      autoFocus={true}
+                      placeholder="Your Title"
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                  <div className="input-field">
+                    <label htmlFor="content">Content</label>
+                    <textarea
+                      id="content"
+                      rows="6"
+                      maxLength={150}
+                      placeholder="Your Content"
+                      onChange={this.handleChange}
+                    ></textarea>
+                  </div>
 
-          <div className="input-field">
-            <button className="">Create</button>
-          </div>
-        </form>
+                  <div className="input-field">
+                    <button className="auth-button">Create</button>
+                  </div>
+                </div>
+              </form>
+            </CardContent>
+          </CustomCard>
+      </div>
       </div>
     );
   }
