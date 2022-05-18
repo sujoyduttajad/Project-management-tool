44876f9d70ab1d1db9926d44174d9988cad18cdb
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-05-18 14:43:35 +0530

    Update ProjectList.js

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,10 +1,36 @@
 import React from "react";
 import ProjectSummary from "./ProjectSummary";
 import { Link } from "react-router-dom";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import { styled } from "@mui/material/styles";
+
+
+export const CustomSearch = styled(TextField)({
+  fontFamily: "'Noto Sans', sans-serif",
+  maxWidth: "60em",
+  width: "100%",
+  fontSize: '1em',
+  "&:hover": {
+    boxShadow: "-1px 3px 8px 0px rgba(56,56,56,0.91)",
+  },
+});
 
 function ProjectList({ projects }) {
+
+  console.log(projects)
+
   return (
     <section className="project-list-section">
+      <div className="serach-container">
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={projects}
+          sx={{ width: "100%", fontFamily: "'Noto Sans', sans-serif", fontWeight: 500 }}
+          renderInput={(params) => <CustomSearch {...params} label="Projects" />}
+        />
+      </div>
       {projects &&
         projects.map((project) => {
           return (
