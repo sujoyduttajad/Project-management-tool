fc8b729af05c725a5e3bece996786d28acca828a
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-05-19 14:57:22 +0530

    Create SidePanel.js

---
+++ src/components/layout/SidePanel.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SidePanel = () => {
+  return (
+    <nav className="panel-wrapper">
+      <div className="container">Panel Bar</div>
+    </nav>
+  );
+};
+
+export default SidePanel;
