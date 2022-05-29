3040c61690ff6e434478d0908461d8f083d834f1
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-05-29 17:31:02 +0530

    Update SidePanel.js

--- src/components/layout/SidePanel.js
+++ src/components/layout/SidePanel.js
@@ -1,28 +1,61 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
 import { Redirect } from "react-router-dom";
 import { signOut } from "../../store/actions/authActions";
-import { Avatar } from "@mui/material";
+import { Avatar, Badge } from "@mui/material";
+import { styled } from "@mui/material/styles";
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 import { NavLink } from "react-router-dom";
 import { StyledChip, SecondaryButton } from "./SignedInLinks";
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    backgroundColor: "#44b700",
+    color: "#44b700",
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    "&::after": {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      borderRadius: "50%",
+      animation: "ripple 1.2s infinite ease-in-out",
+      border: "1px solid currentColor",
+      content: '""',
+    },
+  },
+  "@keyframes ripple": {
+    "0%": {
+      transform: "scale(.8)",
+      opacity: 1,
+    },
+    "100%": {
+      transform: "scale(2.4)",
+      opacity: 0,
+    },
+  },
+}));
 
 const SidePanel = (props) => {
   const [dropOpen, setDropOpen] = useState(false);
 
   const { projects, auth, profile } = props;
-  if (!auth.uid) return <Redirect to="/signin" />;
+  // if (!auth.uid) return <Redirect to="/signin" />;
 
   let userList;
-  if (projects) {
-    userList = new Set(projects.map((user) => user.authorFirstName));
+  const userArray = async () => {
+    
+    if (projects) {
+      userList = await new Set(projects.map((user) => user.authorFirstName));
+    }
   }
+ 
+  userArray();
 
   console.log(userList);
-
-  
 
   return (
     <nav className="panel-wrapper">
@@ -69,6 +102,28 @@ const SidePanel = (props) => {
         <div className="users-list-container">
           <h2>All Users</h2>
           <hr />
+          <div>
+            {userList &&
+              userList.map((user, index) => (
+                <StyledBadge
+                  key={index}
+                  overlap="circular"
+                  anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+                  variant="dot"
+                >
+                  <Avatar
+                    variant="circular"
+                    sx={{
+                      bgcolor: "#131E3F",
+                      lineHeight: 0,
+                    }}
+                    alt={user}
+                  >
+                    {user}
+                  </Avatar>
+                </StyledBadge>
+              ))}
+          </div>
         </div>
       </div>
     </nav>
